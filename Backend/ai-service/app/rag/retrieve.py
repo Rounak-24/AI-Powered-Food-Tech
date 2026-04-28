6f94864031ba46ff405ea1e7e4ef856b6a992d2d
Rounak Mitra <rounakmitra9@gmail.com>
2026-04-28 13:13:10 +0530

    fixing internal_services, vision agent

--- Backend/ai-service/app/rag/retrieve.py
+++ Backend/ai-service/app/rag/retrieve.py
@@ -3,6 +3,7 @@ from app.rag.index import vector_store
 async def get_vector_search_result(user_id:str, user_query:str, filename:str):
     return await vector_store.similarity_search(
         query = user_query,
+        k = 2,
         filter = {
             "user_id":user_id,
             "filename":filename
--- Backend/ai-service/app/services/internal_service.py
+++ Backend/ai-service/app/services/internal_service.py
@@ -1,45 +1,24 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+import asyncio
 import httpx
 from json import dumps
 
-async def get_data_from_inventory(
-    user_id:str,
-    status:str
-):
+async def get_inventory(user_id:str):
     try:
         async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url = os.getenv(f"GET_INVENTORY_API/{status}"),
-                json = {
-                    "userId": user_id
-                },
+            response = await client.get(
+                url = os.getenv(f"INTERNAL_BASE_URL/user-inventory/{user_id}"),
                 timeout=5.0
             )
 
             if response.status_code == 200:
                 data = response.json()
+                print(data)
                 return dumps[data]
             
             return "Recipe API returned an error."
     except Exception as e:
         return f"Internal API unavailable: {str(e)}"
     
-
-async def get_items_tobe_expire(user_id:str):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url = os.getenv("GET_INVENTORY_TOBE_EXPIRE_API"),
-                json = {
-                    "userId": user_id,
-                },
-                timeout=5.0
-            )
-
-            if response.status_code == 200:
-                data = response.json()
-                return dumps[data]
-            
-            return "Internal API returned an error."
-    except Exception as e:
-        return f"Internal API unavailable: {str(e)}"
