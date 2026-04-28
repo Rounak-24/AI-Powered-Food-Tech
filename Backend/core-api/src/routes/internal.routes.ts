fba8691dd061e0146cd7a7989e48338bfaaee426
anniemandal24 <anniemandal24@gmail.com>
2026-04-28 18:58:07 +0530

    changes in internal routes

--- Backend/core-api/src/routes/internal.routes.ts
+++ Backend/core-api/src/routes/internal.routes.ts
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import{
-    getItemByStatus,getItemByStatusAndCategory,getToBeExpiredItems
+    getItemByStatus,getItemByStatusAndCategory,getToBeExpiredItems,getAvailableInventory
 } from "../controllers/internal.controller.js"
 import { jwtAuthMiddleware } from "../middlewares/jwt.middleware.js";
 
 const router=Router()
+router.use(jwtAuthMiddleware)
+
+router.get("/user_inventory/:user_id",getAvailableInventory)
 
 router.get("/to-be-expire",getToBeExpiredItems)
 
