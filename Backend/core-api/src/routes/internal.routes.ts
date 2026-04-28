6d88ce237864d46c340c4ebc2dd6d4529fb5fc49
anniemandal24 <anniemandal24@gmail.com>
2026-04-28 19:59:42 +0530

    internal routes

--- Backend/core-api/src/routes/internal.routes.ts
+++ Backend/core-api/src/routes/internal.routes.ts
@@ -2,10 +2,9 @@ import {Router} from "express";
 import{
     getItemByStatus,getItemByStatusAndCategory,getToBeExpiredItems,getAvailableInventory
 } from "../controllers/internal.controller.js"
-import { jwtAuthMiddleware } from "../middlewares/jwt.middleware.js";
 
 const router=Router()
-router.use(jwtAuthMiddleware)
+
 
 router.get("/user_inventory/:user_id",getAvailableInventory)
 
